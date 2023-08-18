c628c16590ff223d844f3225a750eb58aa24e001
ShinhyoBelok <shibo6937@gmail.com>
2023-08-17 20:03:29 -0400

    Add index and id routes for houses router

--- routes/houses.js
+++ routes/houses.js
@@ -1,7 +1,30 @@
 const express = require('express')
-const mongoose = require('mongoose')
-const House = require("../database/House_schema")
+const router = express.Router()
+const House = require("../database/model/HouseM")
 
-// app.get('/', (req,res) => {
+router.get('/', async (req, res) => {
+  try {
+    const houses = await House.find()
+    res.send(houses)
+  } catch (error) {
+    console.error('Error fetching houses', error)
+    res.status(500).send('Error fetching houses')
+  }
+})
 
-// })
+router.route('/:id')
+  .get((req, res) => {
+    res.send(req.house)
+  })
+
+router.param('id', async (req, res, next, id) => {
+  try {
+    req.house = await House.findById(id)
+  } catch (error) {
+    console.error(`Error fetching house by id: ${id} \n \n`, error)
+    res.status(500).send('Error fetching house by id')
+  }
+  next()
+})
+
+module.exports = router
