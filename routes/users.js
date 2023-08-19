fae7cb52c382c1b1d84f670618f2aceba35bf4d6
ShinhyoBelok <shibo6937@gmail.com>
2023-08-19 09:24:15 -0400

    Add users router

---
+++ routes/users.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const router = express.Router()
+const User = require('../database/model/UserModel')
+
+router.post('login', (req, res) => {
+  const { username } = req.body
+  const newUser = new User({ username })
+
+  if (User.findOne(newUser)) {
+    res.status(422).json({ message: 'Username already exist' }) //status Unprocessable content
+  } else if (newUser.save()) {
+    res.status(201).json({ user: newUser }) //status created
+  } else {
+    res.status(500).json({ errors: newUser.errors })
+  }
+})
