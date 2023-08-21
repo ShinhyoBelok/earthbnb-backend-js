e94f1babc30432cfc391108f7314e93b2397f16d
ShinhyoBelok <shibo6937@gmail.com>
2023-08-21 18:02:43 -0400

    Add reservations router

--- routes/reservations.js
+++ routes/reservations.js
@@ -1,12 +1,29 @@
 const express = require('express')
 const router = express.Router()
 const Reservation = require('../database/model/ReservationModel')
+const User = require('../database/model/UserModel')
 
 router.post('/', (req, res) => {
-  const newReservation = new Reservation(req.body.newReservation)
+  const newReservation = new Reservation(req.body)
+  console.log(newReservation);
   if (newReservation.save()) {
-    res.status(201).json({ json: newReservation })
+    res.status(201).send({ json: newReservation })
   } else {
-    res.status(422).json({ errors: newReservation.errors })
+    res.status(422).send({ errors: newReservation.errors })
   }
 })
+
+router.get('/:username', async (req, res) => {
+  try {
+    const user = await User.find({ username: req.params.username })
+    const reservations = await Reservation.find({ user_id: user })
+      .populate('house_id')
+      .populate('user_id')
+    console.log(reservations);
+    res.send(reservations)
+  } catch (err) {
+    res.send([])
+  }
+})
+
+module.exports = router
