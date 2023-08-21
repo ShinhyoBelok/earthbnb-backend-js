56aa4ea98f7d613147fab3201cda50f5f62a9853
ShinhyoBelok <shibo6937@gmail.com>
2023-08-21 18:03:33 -0400

    Add reservations, login, logout routers

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
 require('./database/mongoose')
 const express = require('express')
+const cors = require('cors');
 const app = express()
 const port = 3000
 const housesRouter = require('./routes/houses')
 const usersRouter = require('./routes/users')
+const reservationsRouter = require('./routes/reservations')
+const loginRouter = require('./routes/login')
+const logoutRouter = require('./routes/logout')
 
 app.listen(port,() => {
   console.log('Listening to port 3000')
 })
 
+app.use(cors({
+  origin: 'http://localhost:5173'
+}));
+
 app.use( express.json() )
 app.use('/houses', housesRouter)
 app.use('/users', usersRouter)
+app.use('/reservations', reservationsRouter)
+app.use('/login', loginRouter)
+app.use('/logout', logoutRouter)
